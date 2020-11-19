fc58b0d6e96ee35b008d664f19f48ea57bec1df7
ChristianJaziel <71855314+ChristianJaziel@users.noreply.github.com>
2020-11-18 20:38:09 -0600

    Add files via upload

---
+++ principal.java
@@ -0,0 +1,121 @@
+package proyecto2;
+
+public class principal {
+public static void main (String a[] ){
+	punto figura [] = {
+			 	
+	            new punto(400, 250),
+	            new punto(400, 200),
+	            new punto(300, 200),
+	            new punto(300, 250),
+	            new punto(400, 250),
+	            
+	            new punto(450, 250),
+	            new punto(450, 300),
+	            new punto(400, 300),
+	            new punto(400, 250),
+	            
+	            new punto(450, 300),
+	            new punto(500, 300),
+	            new punto(500, 350),
+	            new punto(450, 350),
+	            new punto(450, 300),
+	            
+	            new punto(500, 300),
+	            new punto(500, 250),
+	            new punto(550, 250),
+	            new punto(550, 300),
+	            new punto(500, 300),
+	            
+	            new punto(550, 250),
+	            new punto(550, 200),
+	            new punto(650, 200),
+	            new punto(650, 250),
+	            new punto(550, 250),
+	            
+	            new punto(650, 250),
+	            new punto(700, 250),
+	            new punto(700, 300),
+	            new punto(650, 300),
+	            new punto(650, 250),
+	            
+	            new punto(700, 300), 
+	            new punto(750, 300),
+	            new punto(750, 450),
+	            new punto(700, 450),
+	            new punto(700, 300),
+	            
+	            new punto(700, 450),
+	            new punto(700, 500),
+	            new punto(650, 500),
+	            new punto(650, 450),
+	            new punto(700, 450),
+	            
+	            new punto(650, 500),
+	            new punto(650, 550),
+	            new punto(600, 550),
+	            new punto(600, 500),
+	            new punto(650, 500),
+	            
+	            new punto(600, 550),
+	            new punto(600, 600),
+	            new punto(550, 600),
+	            new punto(550, 550),
+	            new punto(600, 550),
+	            
+	            new punto(550, 600),
+	            new punto(550, 650),
+	            new punto(500, 650),
+	            new punto(500, 600),
+	            new punto(550, 600),
+	            
+	            new punto(500, 650),
+	            new punto(500, 700),
+	            new punto(450, 700),
+	            new punto(450, 650),
+	            new punto(500, 650),
+	           
+	            new punto(450, 650),
+	            new punto(450, 600),
+	            new punto(400, 600),
+	            new punto(400, 650),
+	            new punto(450, 650),
+	            
+	            new punto(400, 600),
+	            new punto(400, 550),
+	            new punto(350, 550),
+	            new punto(350, 600),
+	            new punto(400, 600),
+	            
+	            new punto(350, 550),
+	            new punto(350, 500),
+	            new punto(300, 500),
+	            new punto(300, 550),
+	            new punto(350, 550),
+	            
+	            new punto(300, 500),
+	            new punto(300, 450),
+	            new punto(250, 450),
+	            new punto(250, 500),
+	            new punto(300, 500),
+	            
+	            new punto(250, 450),
+	            new punto(200, 450),
+	            new punto(200, 300),
+	            new punto(250, 300),
+	            new punto(250, 450),
+	            
+	            new punto(250, 300),
+	            new punto(250, 250),
+	            new punto(300, 250),
+	            new punto(300, 300),
+	            new punto(250, 300),
+	            new punto(300, 250),
+	            
+	};
+	
+	   //Crear instancia de ventana
+    new ventana("Transformaciones geometricas", figura);
+}
+
+}
